544a6a7faee81b7dbe6e07c76d6262315c254dad
Diggo Silva <rdiggosilva@gmail.com>
2026-01-24 11:09:00 -0300

    Start configuration

---
+++ FakeStoreAPI/App/Scenes/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FakeStoreAPI
+//
+//  Created by Diggo Silva on 24/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
