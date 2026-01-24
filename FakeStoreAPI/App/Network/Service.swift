68b250b69adc36407879d8bbbc7f7be5eec527bc
Diggo Silva <rdiggosilva@gmail.com>
2026-01-24 12:21:25 -0300

    Created Service network

--- FakeStoreAPI/App/Network/Service.swift
+++ FakeStoreAPI/App/Network/Service.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+enum ServiceError: Error {
+    case invalidURL
+    case invalidResponse
+    case decodeError
+}
+
+protocol ServiceProtocol: AnyObject {
+    func getProducts() async throws -> [Product]
+}
+
+final class Service: ServiceProtocol {
+    func getProducts() async throws -> [Product] {
+        let urlString = "https://fakestoreapi.com/products"
+        
+        guard let url = URL(string: urlString) else { throw ServiceError.invalidURL }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw ServiceError.invalidResponse }
+        
+        let productResponse = try JSONDecoder().decode([ProductResponse].self, from: data)
+        
+        let products = productResponse.map { item in
+            return Product(
+                id: item.id,
+                title: item.title,
+                price: item.price,
+                descriptionProduct: item.descriptionProduct,
+                category: Category(rawValue: item.category.rawValue) ?? .unknown,
+                image: item.image
+            )
+        }
+        return products
+    }
+}
