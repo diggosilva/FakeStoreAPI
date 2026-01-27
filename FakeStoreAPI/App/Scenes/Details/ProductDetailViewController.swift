4b454f5c831d48ca2ca3cb7506585c8a2b6c1fe3
Diggo Silva <rdiggosilva@gmail.com>
2026-01-27 09:00:30 -0300

    Created ProductDetailViewController and connected with View and ViewModel

--- FakeStoreAPI/App/Scenes/Details/ProductDetailViewController.swift
+++ FakeStoreAPI/App/Scenes/Details/ProductDetailViewController.swift
@@ -5,4 +5,36 @@
 //  Created by Diggo Silva on 26/01/26.
 //
 
-import Foundation
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+    
+    private let contentView = ProductDetailView()
+    private let viewModel: ProductDetailViewModelProtocol
+    
+    init(viewModel: ProductDetailViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
+    
+    override func loadView() {
+        view = contentView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigationBar()
+        bindViewModel()
+    }
+    
+    private func configureNavigationBar() {
+        navigationItem.title = "Detalhes do Produto"
+    }
+    
+    private func bindViewModel() {
+        let product = viewModel.updateProduct()
+        contentView.configure(product: product)
+    }
+}
