822c507badc8082eeb8ae9668e0ec8ebf738b0da
Diggo Silva <rdiggosilva@gmail.com>
2026-01-27 08:59:34 -0300

    Created ProductDetailViewModel

--- FakeStoreAPI/App/Scenes/Details/ProductDetailViewModel.swift
+++ FakeStoreAPI/App/Scenes/Details/ProductDetailViewModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol ProductDetailViewModelProtocol {
+    func updateProduct() -> Product
+}
+
+final class ProductDetailViewModel: ProductDetailViewModelProtocol {
+    
+    private var product: Product
+    
+    init(product: Product) {
+        self.product = product
+    }
+    
+    func updateProduct() -> Product {
+        return product
+    }
+}
