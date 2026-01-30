a66c3cd75cd355af6bc4c4a71d10a3d73cce3e8a
Diggo Silva <rdiggosilva@gmail.com>
2026-01-30 08:27:28 -0300

    Created CartManager to have control about the cart in all app

--- FakeStoreAPI/App/Scenes/TabBar/MainTabBarController.swift
+++ FakeStoreAPI/App/Scenes/TabBar/MainTabBarController.swift
@@ -6,15 +6,19 @@
 //
 
 import UIKit
+import Combine
 
 final class MainTabBarController: UITabBarController {
     
     private let vc1 = UINavigationController(rootViewController: FeedViewController())
     private let vc2 = UINavigationController(rootViewController: CartViewController())
     
+    private var cancellables = Set<AnyCancellable>()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        observeCart()
     }
     
     func setup() {
@@ -27,5 +31,15 @@ final class MainTabBarController: UITabBarController {
         vc2.tabBarItem.selectedImage = UIImage(systemName: "cart.fill")
         
         viewControllers = [vc1, vc2]
+    }
+    
+    private func observeCart() {
+        CartManager.shared.$items
+            .receive(on: RunLoop.main)
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                let count = CartManager.shared.totalItemsCount
+                self.vc2.tabBarItem.badgeValue = count > 0 ? "\(count)" : nil
+            }.store(in: &cancellables)
     }
 }
